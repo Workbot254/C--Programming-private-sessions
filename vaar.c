59d35807ef9897273081011084a25d008aeb0912
Ernest <ekiromo50@gmail.com>
2023-02-25 01:34:22 -0800

    More variables

---
+++ vaar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - start of program execution
+ * Return: always 0
+ */
+int main(void)
+{
+	int var1, var2, var3, var4;
+
+	var1 = var2 = var3 = var4 = 17;
+
+	printf("The value of var1 is %d\n", var1);
+	printf("The value of var2 is %d\n", var2);
+	printf("The value of var3 is %d\n", var3);
+	printf("The value of var4 is %d\n", var4);
+	return (0);
+}
