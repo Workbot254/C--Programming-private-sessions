425ea3302079509e66cd4911f54b3a1982598404
Ernest <ekiromo50@gmail.com>
2023-02-25 01:08:21 -0800

    Hello world and variables

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - start pf code compilation
+*Return: Always 0
+*/
+int main(void)
+{
+printf("Hello world\n");
+return (0);
+}
---
+++ var.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Function starts executing
+ * Return: Always 0;
+ */
+int main(void)
+{
+	int var = 3;
+
+	var = 4;
+
+	printf("Thavalue of variable var is %d\n", var);
+	return (0);
+}
